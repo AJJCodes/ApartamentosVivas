522f249e0ff6ca3e518cefac17ad08375e22f601
Anthony Jackson <anthonyj300@yahoo.com>
2023-11-19 23:23:52 -0600

    Integracion del Dashboard y controlador Base para todo rol

---
+++ Web/Controllers/Home/DashBoardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Controllers.Seguridad;
+using Web.Filters;
+
+namespace Web.Controllers.Home
+{
+    [VerificaSession]
+    public class DashBoardController : BaseController
+    {
+        // GET: DashBoard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
