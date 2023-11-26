fe35deb2297392b315782e17dcf6ecfa52a3c9f6
Anthony Jackson <anthonyj300@yahoo.com>
2023-11-25 22:38:06 -0600

    Actualizaciones Dudosas

--- Web/Controllers/Seguridad/AccesoController.cs
+++ Web/Controllers/Seguridad/AccesoController.cs
@@ -45,7 +45,7 @@ namespace Web.Controllers.Seguridad
 
             Session["User"] = oUser;
             ViewBag.UserName = oUser.Nombre + " " + oUser.Apellidos;
-            return RedirectToAction("Index", "Dashboard");
+            return RedirectToAction("Index", "Home");
         }
 
 
